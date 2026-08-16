8d950c1141370f5980ca9a5707e36212b4b05f09
igor5200 <59863854+igor5200@users.noreply.github.com>
2026-08-16 21:18:07 +0200

    Update drone5.lua

--- drone5.lua
+++ drone5.lua
@@ -1,127 +1,88 @@
--- =========================================================
--- GYROSCOPIC PROPELLER BEARING TESTER
--- =========================================================
+local front = peripheral.wrap("gyroscopic_propeller_bearing_0")
+local back  = peripheral.wrap("gyroscopic_propeller_bearing_1")
+local left  = peripheral.wrap("gyroscopic_propeller_bearing_2")
+local right = peripheral.wrap("gyroscopic_propeller_bearing_3")
 
-local FRONT = peripheral.wrap("gyroscopic_propeller_bearing_0")
-local BACK  = peripheral.wrap("gyroscopic_propeller_bearing_1")
-local LEFT  = peripheral.wrap("gyroscopic_propeller_bearing_2")
-local RIGHT = peripheral.wrap("gyroscopic_propeller_bearing_3")
+local selected = front
+local angle = 0
 
-local bearings = {
-    front = FRONT,
-    back = BACK,
-    left = LEFT,
-    right = RIGHT
-}
-
-local selected = "front"
-
--- =========================================================
--- USTAWIENIE CELU
--- =========================================================
-
-local function setBearing(bearing, x, y, z)
-    bearing.setManualTarget({
-        x = x,
-        y = y,
-        z = z
-    })
-end
-
--- =========================================================
--- RESET
--- =========================================================
-
-local function resetAll()
-    for _, bearing in pairs(bearings) do
-        if bearing then
-            setBearing(bearing, 0, 0, 0)
-        end
-    end
-end
-
--- =========================================================
--- INFO
--- =========================================================
-
-local function printInfo()
+while true do
     term.clear()
     term.setCursorPos(1, 1)
 
-    print("=== GYROSCOPIC BEARING TESTER ===")
+    print("GYROSCOPIC BEARING TESTER")
+    print("-------------------------")
+    print("1 - FRONT")
+    print("2 - BACK")
+    print("3 - LEFT")
+    print("4 - RIGHT")
     print("")
-    print("Wybrany: " .. selected)
+    print("A - X -10")
+    print("D - X +10")
+    print("W - Y +10")
+    print("S - Y -10")
+    print("Q - CENTER")
+    print("E - EXIT")
     print("")
-    print("f = FRONT")
-    print("b = BACK")
-    print("l = LEFT")
-    print("r = RIGHT")
-    print("")
-    print("a = -30")
-    print("d = +30")
-    print("s = CENTER")
-    print("")
-    print("q = EXIT")
-end
-
--- =========================================================
--- START
--- =========================================================
-
-resetAll()
-
-while true do
-
-    printInfo()
+    print("Angle: " .. angle)
 
     local _, key = os.pullEvent("key")
 
-    -- EXIT
-    if key == keys.q then
-        resetAll()
+    if key == keys.one then
+        selected = front
+
+    elseif key == keys.two then
+        selected = back
+
+    elseif key == keys.three then
+        selected = left
+
+    elseif key == keys.four then
+        selected = right
+
+    elseif key == keys.a then
+        angle = angle - 10
+        selected.setManualTarget({
+            x = angle,
+            y = 0,
+            z = 0
+        })
+
+    elseif key == keys.d then
+        angle = angle + 10
+        selected.setManualTarget({
+            x = angle,
+            y = 0,
+            z = 0
+        })
+
+    elseif key == keys.w then
+        selected.setManualTarget({
+            x = 0,
+            y = 10,
+            z = 0
+        })
+
+    elseif key == keys.s then
+        selected.setManualTarget({
+            x = 0,
+            y = -10,
+            z = 0
+        })
+
+    elseif key == keys.q then
+        selected.setManualTarget({
+            x = 0,
+            y = 0,
+            z = 0
+        })
+        angle = 0
+
+    elseif key == keys.e then
+        front.setManualTarget({x = 0, y = 0, z = 0})
+        back.setManualTarget({x = 0, y = 0, z = 0})
+        left.setManualTarget({x = 0, y = 0, z = 0})
+        right.setManualTarget({x = 0, y = 0, z = 0})
         break
     end
-
-    -- WYBÓR
-    if key == keys.f then
-        selected = "front"
-
-    elseif key == keys.b then
-        selected = "back"
-
-    elseif key == keys.l then
-        selected = "left"
-
-    elseif key == keys.r then
-        selected = "right"
-
-    -- -30
-    elseif key == keys.a then
-        setBearing(
-            bearings[selected],
-            -30,
-            0,
-            0
-        )
-
-    -- +30
-    elseif key == keys.d then
-        setBearing(
-            bearings[selected],
-            30,
-            0,
-            0
-        )
-
-    -- CENTER
-    elseif key == keys.s then
-        setBearing(
-            bearings[selected],
-            0,
-            0,
-            0
-        )
-    end
 end
-
-print("Tester zakonczony.")
